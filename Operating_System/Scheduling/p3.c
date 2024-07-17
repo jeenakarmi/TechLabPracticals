f6bbda6c3bf4fb04a37a723db92a78093ff083fd
jeenakarmi <jinanakarmi@gmail.com>
2024-07-17 21:46:53 +0545

    lab3

--- Operating_System/Scheduling/p3.c
+++ Operating_System/Scheduling/p3.c
@@ -2,60 +2,85 @@
 
 #include <stdio.h>
 
-void main() {
-    int bt[15], wt[15], tat[15], ct[15], at[15], p[15], n, i, j;
-    float awt, atat, twt = 0, ttat = 0;
-    
-    // Asking for the number of processes
-    printf("Enter the number of processes:\n");
+struct Process
+{
+    int p_id;
+    int at;
+    int bt;
+    int ct;
+    int tat;
+    int wt;
+};
+
+int main()
+{
+    int n = 0;
+    // get no of processes
+    printf("Enter no. of processes: ");
     scanf("%d", &n);
-    
-    // Getting burst time for each process
-    for (i = 0; i < n; i++) {
-        printf("Enter burst time for process p%d: ", i);
-        scanf("%d", &bt[i]);
-        at[i] = 0; // Since all processes arrive at time 0
-        p[i] = i;  // Initialize process index array
+    Process processes[n];
+   
+    // get burst time for processes
+    for (int i = 0; i < n; ++i)
+    {
+        processes[i].p_id = i;
+        processes[i].at = 0;
+        printf("Enter burst time for p%d: ", i);
+        scanf("%d", &processes[i].bt);
     }
-    
-    // Sort processes based on burst time using bubble sort
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (bt[j] > bt[j + 1]) {
-                // Swap burst time
-                int temp = bt[j];
-                bt[j] = bt[j + 1];
-                bt[j + 1] = temp;
-                
-                // Swap process index
-                temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+
+    // sort processes according to burst time
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            if (processes[j].bt > processes[j + 1].bt) {
+                Process temp = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp;
+            }
+        }
+    }
+
+    // running time of processes
+    int running_time = 0;
+    int sum_tat = 0;
+    int sum_wt = 0;
+    printf("process\t\tat\t\tbt\t\tct\t\ttat\t\twt\n");
+    for (int i = 0; i < n; ++i)
+    {
+        running_time += processes[i].bt;
+        processes[i].ct = running_time;
+        processes[i].tat = running_time; // arrival time is 0
+        processes[i].wt = processes[i].tat - processes[i].bt;
+        sum_tat += processes[i].tat;
+        sum_wt += processes[i].wt;
+    }
+
+    // sort processes according to process id
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            if (processes[j].p_id > processes[j + 1].p_id) {
+                Process temp = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp;
             }
         }
     }
     
-    int tempT = 0;
-    for (i = 0; i < n; i++) {
-        ct[i] = bt[i] + tempT;
-        tempT = ct[i];
-        
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-        
-        twt += wt[i];
-        ttat += tat[i];
+    // display calculation table
+    for (int i = 0; i < n; ++ i)
+    {
+        printf("p%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i, processes[i].at, processes[i].bt, processes[i].ct, processes[i].tat, processes[i].wt);
     }
-    
-    awt = (twt / n);
-    atat = (ttat / n);
-    
-    // Printing the results
-    printf("Process\t\tBT\t\tAT\t\tCT\t\tTAT\t\tWT\n");
-    for (i = 0; i < n; ++i) {
-        printf("p%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], at[i], ct[i], tat[i], wt[i]);
-    }
-    printf("Average Waiting Time: %f\n", awt);
-    printf("Average Turnaround Time: %f\n", atat);
-}
 
+    float atat = sum_tat / n;
+    float awt = sum_wt / n;
+
+    printf("Avg TAT: %f\n", atat);
+    printf("Avg WT: %f\n", awt);
+
+    return 0;
+}
